1f20aec01abe6e13923af639c711d59d542f870a
Peterson Fonseca <petersonsimiao1@gmail.com>
2021-05-15 18:01:33 -0300

    feat(TaskList): criado componente e stories

---
+++ src/components/TaskList/TaskList.stories.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+import TaskList from "./TaskList";
+import { Default as TaskStories } from "../Task/Task.stories";
+
+export default {
+  component: TaskList,
+  title: "Task list",
+  decorators: [(story) => <div style={{ padding: "3rem" }}>{story()}</div>],
+};
+
+const Template = (args) => <TaskList {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  tasks: [
+    { ...TaskStories.args.task, id: "1", title: "Task 1" },
+    { ...TaskStories.args.task, id: "2", title: "Task 2" },
+    { ...TaskStories.args.task, id: "3", title: "Task 3" },
+    { ...TaskStories.args.task, id: "4", title: "Task 4" },
+    { ...TaskStories.args.task, id: "5", title: "Task 5" },
+    { ...TaskStories.args.task, id: "6", title: "Task 6" },
+  ],
+};
+
+export const WithPinnedTasks = Template.bind({});
+WithPinnedTasks.args = {
+  tasks: [
+    ...Default.args.tasks.slice(0, 5),
+    { id: "6", title: "Task 6 (pinned)", state: "TASK_PINNED" },
+  ],
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+  tasks: [],
+  loading: true,
+};
+
+export const Empty = Template.bind({});
+Empty.args = {
+  tasks: [],
+  loading: false,
+};
